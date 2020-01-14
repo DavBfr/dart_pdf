b45573df1978cd71196af1e11824ce073aa66390
David PHAM-VAN <dev.nfet.net@gmail.com>
2020-01-14 09:53:47 -0500

    Update example with latest template

---
+++ printing/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import printing
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+    PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
+}
