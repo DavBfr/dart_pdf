f4520a87bb09618249258541ddd04d2cfac879a2
David PHAM-VAN <dev.nfet.net@gmail.com>
2021-09-10 11:23:54 -0300

    Fix example lints

--- printing/example/lib/main.dart
+++ printing/example/lib/main.dart
@@ -12,7 +12,7 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp(this.title);
+  const MyApp(this.title, {Key? key}) : super(key: key);
 
   final String title;
 
