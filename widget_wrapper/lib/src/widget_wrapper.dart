e601174d5820f40ad0f5ab241e732423738792ab
Adil Hanney <adilh+github@orchid.anonaddy.com>
2024-02-16 15:08:14 +0000

    fix: correct use of Flutter 3.19's ViewConfiguration constructor

--- widget_wrapper/lib/src/widget_wrapper.dart
+++ widget_wrapper/lib/src/widget_wrapper.dart
@@ -170,13 +170,9 @@ class WidgetWrapper extends pw.ImageProvider {
       child: RenderPositionedBox(
           alignment: Alignment.center, child: repaintBoundary),
       configuration: ViewConfiguration(
-          physicalConstraints: BoxConstraints(
-            maxWidth: computedConstraints.maxWidth,
-            maxHeight: computedConstraints.maxHeight,
-          ),
-          logicalConstraints: BoxConstraints(
-            maxWidth: computedConstraints.maxWidth,
-            maxHeight: computedConstraints.maxHeight,
+          size: Size(
+            computedConstraints.maxWidth,
+            computedConstraints.maxHeight,
           ),
           devicePixelRatio: view.devicePixelRatio),
       view: view,
