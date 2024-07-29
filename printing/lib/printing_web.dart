e326e36c4975485c92299aa04315c767537cf150
Sabin Neupane <61322712+sabin26@users.noreply.github.com>
2024-07-29 15:52:26 +0545

    Update web dependency

--- printing/lib/printing_web.dart
+++ printing/lib/printing_web.dart
@@ -213,7 +213,8 @@ class PrintingPlugin extends PrintingPlatform {
       script.setAttribute('id', _scriptId);
       script.setAttribute('type', 'text/javascript');
       script.innerHTML =
-          '''function ${_frameId}_print(){var f=document.getElementById('$_frameId');f.focus();f.contentWindow.print();}''';
+          '''function ${_frameId}_print(){var f=document.getElementById('$_frameId');f.focus();f.contentWindow.print();}'''
+              .toJS;
       doc.body!.append(script);
 
       final frame = doc.getElementById(_frameId) ?? doc.createElement('iframe');
