ba07f78c30a14dba95c856dbf967616e36bf0532
João Carvalho <joao.carvalho@present-technologies.com>
2026-03-25 21:16:53 +0000

    Fixes web plugin compile issue

--- printing/lib/printing_web.dart
+++ printing/lib/printing_web.dart
@@ -159,6 +159,7 @@ class PrintingPlugin extends PrintingPlatform {
     bool usePrinterSettings,
     OutputType outputType,
     bool forceCustomPrintPaper,
+    bool windowsModernDialog,
   ) async {
     late Uint8List result;
     try {
