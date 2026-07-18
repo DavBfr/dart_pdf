1dfba0d821d28b26598f6b8f3be0a09dfedabf98
David PHAM-VAN <dev.nfet.net@gmail.com>
2026-07-18 07:41:45 -0300

    Fix CORS

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "492810319" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2342729587" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
